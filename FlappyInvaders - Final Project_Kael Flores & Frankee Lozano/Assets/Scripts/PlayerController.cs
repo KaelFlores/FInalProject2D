508fe4770fa94cadb7fc02fc75aa0c4b5217b10f
Flores <705253@student.dusd.net>
2022-05-09 09:50:46 -0700

    Script

--- FlappyInvaders - Final Project_Kael Flores & Frankee Lozano/Assets/Scripts/PlayerController.cs
+++ FlappyInvaders - Final Project_Kael Flores & Frankee Lozano/Assets/Scripts/PlayerController.cs
@@ -5,16 +5,29 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Animator animator;
+    Rigidbody2D rb2D;
+
+    float vertical;
+    public float speed = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = gameObject.GetComponent<Animator>();
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        vertical = Input.GetAxis("Vertical");
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = rb2D.position;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+        rb2D.MovePosition(position);
     }
 }
