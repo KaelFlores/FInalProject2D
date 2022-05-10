59db4b26398d12605e72d214f6b534c0a5cec454
Lozano <741039@student.dusd.net>
2022-05-10 09:09:07 -0700

    repeat bg script

--- FlappyInvaders - Final Project_Kael Flores & Frankee Lozano/Assets/Scripts/RepeatingBackground.cs
+++ FlappyInvaders - Final Project_Kael Flores & Frankee Lozano/Assets/Scripts/RepeatingBackground.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class RepeatingBackground : MonoBehaviour
 {
+    private BoxCollider2D groundCollider;
+    private float groundHorizontalLength;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        groundCollider = GetComponent<BoxCollider2D>();
+        groundHorizontalLength = groundCollider.size.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x < -groundHorizontalLength)
+        {
+            RepositionBackground();
+        }
+    }
+
+    private void RepositionBackground()
+    {
+        Vector2 groundOffSet = new Vector2(groundHorizontalLength * 2.5f, 0);
+        transform.position = (Vector2)transform.position + groundOffSet;
     }
 }
