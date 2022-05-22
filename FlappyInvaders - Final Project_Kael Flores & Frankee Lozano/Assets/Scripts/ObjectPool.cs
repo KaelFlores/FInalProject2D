ba867c4c64552374b105366b3a855637ed0ccb26
KaelFlores <104641960+KaelFlores@users.noreply.github.com>
2022-05-21 18:28:23 -0700

    Update ObjectPool.cs

--- FlappyInvaders - Final Project_Kael Flores & Frankee Lozano/Assets/Scripts/ObjectPool.cs
+++ FlappyInvaders - Final Project_Kael Flores & Frankee Lozano/Assets/Scripts/ObjectPool.cs
@@ -8,17 +8,10 @@ public class ObjectPool : MonoBehaviour
     public static ObjectPool SharedInstance;
 
     private List<GameObject> pooledEnemies;
-    private List<GameObject> pooledBlasters;
-    private List<GameObject> pooledCrossBlasts;
-    private List<GameObject> pooledSparkBlasts;
+    
     public GameObject enemyToPool;
-    public GameObject blasterToPool;
-    public GameObject crossBlastToPool;
-    public GameObject sparkBlastToPool;
+
     public int amountOfEnemiesToPool;
-    public int amountOfBlastersToPool;
-    public int amountOfCrossBlastsToPool;
-    public int amountOfSparkBlastsToPool;
 
     void Awake()
     {
@@ -28,13 +21,9 @@ public class ObjectPool : MonoBehaviour
     void Start()
     {
         pooledEnemies = new List<GameObject>();
-        pooledBlasters = new List<GameObject>();
-        pooledCrossBlasts = new List<GameObject>();
-        pooledSparkBlasts = new List<GameObject>();
+        
         GameObject tmpEnemy;
-        GameObject tmpBlaster;
-        GameObject tmpCrossBlast;
-        GameObject tmpSparkBlast;
+        
         for (int i = 0; i < amountOfEnemiesToPool; i++)
         {
             tmpEnemy = Instantiate(enemyToPool);
